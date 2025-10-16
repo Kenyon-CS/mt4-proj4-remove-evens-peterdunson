76ff230797afaa10fb6fe48635815995bd3abed7
Peter Dunson <90647939+peterdunson@users.noreply.github.com>
2025-10-16 14:06:01 -0400

    submit
    submit, proj4 remove evens

--- src/LinkedList.cpp
+++ src/LinkedList.cpp
@@ -8,7 +8,21 @@ LinkedList::~LinkedList() {
 }
 void LinkedList::push_front(int v) { head_ = new Node(v, head_); }
 void LinkedList::remove_evens() {
-    // TODO: implement
+    while (head_ && head_->value % 2 == 0) {
+        Node* tmp = head_;
+        head_ = head_->next;
+        delete tmp;
+    }
+    Node* p = head_;
+    while (p && p->next) {
+        if (p->next->value % 2 == 0) {
+            Node* dead = p->next;
+            p->next = dead->next;
+            delete dead;
+        } else {
+            p = p->next;
+        }
+    }
 }
 std::string LinkedList::to_string() const {
     std::ostringstream oss;
